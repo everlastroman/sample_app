e3063798b1b34766f743d19baad21d3ee390c80b
everlastroman <everlast.roman@gmail.com>
2021-01-26 21:59:46 +0200

    5 Half of Book ended

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.11.3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+# Use bootsrap 
+gem 'bootstrap-sass', '3.2.0.4'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -12,9 +12,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
-  test "should get about" do
+
+  test "should get About" do
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
     end
+
+  test "should get Contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+   end
 end
