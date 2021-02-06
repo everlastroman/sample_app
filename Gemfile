d180a4163fc51aa366265bd2fd7f2cd56abc26f8
everlastroman <everlast.roman@gmail.com>
2021-02-06 23:46:55 +0200

    finish 9

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 # Use bcrypt for passwords
 gem 'bcrypt', '3.1.7'
+#adding test  users
+gem 'faker', '1.4.2'
+gem 'will_paginate'#, '3.0.7'
+gem 'bootstrap-will_paginate'#, '0.0.10'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,60 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:roman)
+    @other_user = users(:tomas)
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when user not admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
+  test "shoud redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "shoud redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "shoud redirect edit when logged wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "shoud redirect update when logged wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "shoud redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
 end
