9997e3b04d743986c3d3a4db8c495095e8905dc4
everlastroman <everlast.roman@gmail.com>
2021-02-25 03:22:48 +0200

    ex 11

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,25 +9,41 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    assert_no_difference 'Micropost.count' do
-      post microposts_path, micropost: { content: "" }
-    end
-   assert_select 'div#error_explanation'
-   content = "This micropost really ties the room together" 
+    assert_select 'input[type=file]'
+    # Недопустимая информация в форме
+    post microposts_path, micropost: { content: "" }
+    assert_select 'div#error_explanation'
+    # Допустимая информация в форме
+    content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/index.jpeg', 'image/jpeg')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
-    assert_redirected_to root_url
+    assert assigns(:micropost).picture?
     follow_redirect!
     assert_match content, response.body
-    # Удаление сообщения. 
-    assert_select 'a', text: 'delete'
-    first_micropost = @user.microposts.paginate(page: 1).first 
+    # Удаление сообщения.
+    assert_select 'a', 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
-    # Переход в профиль другого пользователя. 
-    get  user_path(users(:archer)) 
-    assert_select 'a', text: 'delete', count: 0
+    # Переход в профиль другого пользователя.
+    get user_path(users(:lana))
+    assert_select 'a', { text: 'delete', count: 0 }
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # У пользователя нет сообщений
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
